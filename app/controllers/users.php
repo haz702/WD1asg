45633b43e0430d1a118787fe9729c0a6e848ee50
Chia9666 <89577865+Chia9666@users.noreply.github.com>
2023-10-25 13:41:01 +0800

    Add files via upload

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -66,9 +66,7 @@ if (isset($_GET["id"])) {
     $user = selectOne($table, ["id" => $_GET["id"]]);
     $id = $user['id'];
     $username = $user['username'];
-
     $admin = $user['admin'] == 1 ? 1 : 0;
-
     $email = $user['email'];
 }
 
